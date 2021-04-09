59fcd8eb327cc7860c72c526f14877d12084a260
Nathaniel Blair-Stahn <ndbs@uw.edu>
2021-04-09 01:58:17 -0700

    uncomment code to run results for deliverable

--- nanosim_models/lbwsg_boe.py
+++ nanosim_models/lbwsg_boe.py
@@ -205,77 +205,77 @@ def main(vivarium_research_lsff_path, out_directory, location, num_simulants, nu
     gap_coverage_proportions = [0.2, 0.5, 0.8] # Low, medium, high scenarios
 #     year = 2025 # Needed in index for Ali's code
 
-#     global_data = data_processing.get_global_data(effect_size_seed, random_seed, draws, take_mean)
-#     fortification_input_data = data_processing.get_fortification_input_data(vivarium_research_lsff_path)
-#     gbd_data = data_processing.get_gbd_input_data()
+    global_data = data_processing.get_global_data(effect_size_seed, random_seed, draws, take_mean)
+    fortification_input_data = data_processing.get_fortification_input_data(vivarium_research_lsff_path)
+    gbd_data = data_processing.get_gbd_input_data()
 
-#     # If take_mean==True, global_data.draws will consist of a single value
-#     output_index = pd.MultiIndex.from_product([global_data.draws, age_group_ids], names=['draw', 'age_group_id'])
-#     output = pd.DataFrame(index=output_index)
+    # If take_mean==True, global_data.draws will consist of a single value
+    output_index = pd.MultiIndex.from_product([global_data.draws, age_group_ids], names=['draw', 'age_group_id'])
+    output = pd.DataFrame(index=output_index)
 
-#     # Now loop through all iron vehicles for location:
-#     # coverage_df = coverage_df.query("location_id == @location_id and nutrient=='iron' and wra==True")
-#     # for vehicle in coverage_df.vehicles.unique():
-#     # Actually, might need to check that vehicle exists in all input dataframes, or exclude some vehicles...
+    # Now loop through all iron vehicles for location:
+    # coverage_df = coverage_df.query("location_id == @location_id and nutrient=='iron' and wra==True")
+    # for vehicle in coverage_df.vehicles.unique():
+    # Actually, might need to check that vehicle exists in all input dataframes, or exclude some vehicles...
 
-#     # Use consumption df to determine which location-vehicle pairs we have data for,
-#     # since it has been inner joined with coverage df
-#     vehicles_for_location = fortification_input_data.consumption.query("location_name==@location")['vehicle'].unique()
-#     for vehicle in vehicles_for_location:
-# #         if vehicle not in vehicles:
-# #             continue
-#         # Get local data for vehicle
-#         local_data = data_processing.get_local_data(global_data, fortification_input_data, location, vehicle)
-#         # Create a calculator and set up its populations
-#         calc = IronBirthweightCalculator(global_data, local_data, gbd_data)
-#         calc.initialize_population_tables(num_simulants, ages)
-#         calc.assign_lbwsg_exposure()
-#         calc.assign_iron_treatment_deleted_birthweights()
+    # Use consumption df to determine which location-vehicle pairs we have data for,
+    # since it has been inner joined with coverage df
+    vehicles_for_location = fortification_input_data.consumption.query("location_name==@location")['vehicle'].unique()
+    for vehicle in vehicles_for_location:
+#         if vehicle not in vehicles:
+#             continue
+        # Get local data for vehicle
+        local_data = data_processing.get_local_data(global_data, fortification_input_data, location, vehicle)
+        # Create a calculator and set up its populations
+        calc = IronBirthweightCalculator(global_data, local_data, gbd_data)
+        calc.initialize_population_tables(num_simulants, ages)
+        calc.assign_lbwsg_exposure()
+        calc.assign_iron_treatment_deleted_birthweights()
 
-#         # Compute population coverage proportions from gap coverage proportions
-#         target_coverage_levels = [
-#             local_data.eats_fortifiable
-#                 + proportion * (local_data.eats_fortifiable - local_data.eats_fortified)
-#             for proportion in gap_coverage_proportions
-#         ]
+        # Compute population coverage proportions from gap coverage proportions
+        target_coverage_levels = [
+            local_data.eats_fortifiable
+                + proportion * (local_data.eats_fortifiable - local_data.eats_fortified)
+            for proportion in gap_coverage_proportions
+        ]
 
-#         # Also compute maximum impact, i.e. impact when coverage = 100% of the population
-#         target_coverage_levels.append(1)
-#         # Compute the corresponding proportion to use as an index key (we could potentially get NaN's...)
-#         proportions = gap_coverage_proportions + [
-#             ((1 - local_data.eats_fortified) / (local_data.eats_fortifiable - local_data.eats_fortified)).mean()
-#         ]
+        # Also compute maximum impact, i.e. impact when coverage = 100% of the population
+        target_coverage_levels.append(1)
+        # Compute the corresponding proportion to use as an index key (we could potentially get NaN's...)
+        proportions = gap_coverage_proportions + [
+            ((1 - local_data.eats_fortified) / (local_data.eats_fortifiable - local_data.eats_fortified)).mean()
+        ]
 
-#         # Compute the PIF for each coverage level
-#         for proportion, target_coverage in zip(proportions, target_coverage_levels):
-#             calc.assign_iron_treated_birthweights(target_coverage)
-#             calc.assign_lbwsg_relative_risks()
-#             pif = calc.calculate_potential_impact_fraction(['draw', 'age_group_id'])
-#             output[(local_data.location_id, vehicle, proportion, 'pif')] = pif
+        # Compute the PIF for each coverage level
+        for proportion, target_coverage in zip(proportions, target_coverage_levels):
+            calc.assign_iron_treated_birthweights(target_coverage)
+            calc.assign_lbwsg_relative_risks()
+            pif = calc.calculate_potential_impact_fraction(['draw', 'age_group_id'])
+            output[(local_data.location_id, vehicle, proportion, 'pif')] = pif
             
-#             bpop, ipop = calc.baseline_pop, calc.intervention_pop
-#             for pop in (bpop, ipop):
-#                 calc.lbwsg_effect.assign_categorical_relative_risk(
-#                     pop, cat_colname='treated_lbwsg_category', rr_colname='lbwsg_relative_risk_for_category'
-#                 )
-#             output[(local_data.location_id, vehicle, proportion, 'categorical_pif')] = (
-#                 potential_impact_fraction(bpop, ipop, 'lbwsg_relative_risk_for_category', ['draw', 'age_group_id'])
+            bpop, ipop = calc.baseline_pop, calc.intervention_pop
+            for pop in (bpop, ipop):
+                calc.lbwsg_effect.assign_categorical_relative_risk(
+                    pop, cat_colname='treated_lbwsg_category', rr_colname='lbwsg_relative_risk_for_category'
+                )
+            output[(local_data.location_id, vehicle, proportion, 'categorical_pif')] = (
+                potential_impact_fraction(bpop, ipop, 'lbwsg_relative_risk_for_category', ['draw', 'age_group_id'])
+            )
+            # Now also do DALYs...
+            averted_dalys = calc.calculate_averted_dalys(['draw', 'age_group_id'], drop_non_groupby_levels=True)
+#             averted_dalys.index = averted_dalys.index.droplevel(
+#                 averted_dalys.index.names.difference(['draw', 'age_group_id'])
 #             )
-#             # Now also do DALYs...
-#             averted_dalys = calc.calculate_averted_dalys(['draw', 'age_group_id'], drop_non_groupby_levels=True)
-# #             averted_dalys.index = averted_dalys.index.droplevel(
-# #                 averted_dalys.index.names.difference(['draw', 'age_group_id'])
-# #             )
-#             output[(local_data.location_id, vehicle, proportion, 'averted_dalys')] = averted_dalys
+            output[(local_data.location_id, vehicle, proportion, 'averted_dalys')] = averted_dalys
 
-#     # Make sure index level names and values match plotting function in Ali's code
-#     if not take_mean:
-#         output.index = output.index.set_levels([f"draw_{n}" for n in draws], level='draw') # age_group_id level stays the same
-#     output.columns = pd.MultiIndex.from_tuples(output.columns, names=['location_id', 'vehicle', 'coverage_level', 'measure'])
+    # Make sure index level names and values match plotting function in Ali's code
+    if not take_mean:
+        output.index = output.index.set_levels([f"draw_{n}" for n in draws], level='draw') # age_group_id level stays the same
+    output.columns = pd.MultiIndex.from_tuples(output.columns, names=['location_id', 'vehicle', 'coverage_level', 'measure'])
 
-#     # Save output with draws as columns and all other identifiers in index
-#     output.unstack('age_group_id').T.to_csv(
-#         f"{out_directory}/iron_bw_results_location_id_{local_data.location_id}.csv")
+    # Save output with draws as columns and all other identifiers in index
+    output.unstack('age_group_id').T.to_csv(
+        f"{out_directory}/iron_bw_results__{location}.csv")
 
 
 if __name__ == "__main__":
